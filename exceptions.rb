8204de8334232900bb02f29d00adce7dc5199941
Will Hopper <whopper@cat.pdx.edu>
2013-04-09 16:04:24 -0700

    Add custom exceptions

---
+++ exceptions.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/ruby
+
+class WrongPlayerError < Exception
+end
+
+class InvalidMoveError < Exception
+end
+
+class MalformedMoveError < Exception
+end
+
+class GameOver < Exception
+end
